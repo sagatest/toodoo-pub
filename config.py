758571ba1b5f30c7202a06c4a3d856747b694248
Gahl Saraf <saraf.gahl@gmail.com>
2022-02-13 14:12:00 +0200

    Toodoo demo initial commit

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@todo-db/app_development" # os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
